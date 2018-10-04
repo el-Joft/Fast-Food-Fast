a108e2fc24ef9e2003ffad9d402b1913dc3f2d1d
el-joft <ottimothy@gmail.com>
2018-10-05 00:42:37 +0100

    [finishes #160821860] Writing Test for all endpoint

--- server/src/helpers/queryHelpers.js
+++ server/src/helpers/queryHelpers.js
@@ -1,7 +1,7 @@
 
 export const findAllOrder = (selectedColumn, tableName) => (`SELECT ${selectedColumn} FROM ${tableName}`);
-export const userText = 'INSERT INTO users(firstName, lastName, email, phone, password, address, city, zipCode) VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *';
-export const orderText = 'INSERT INTO orders(menuId, orderedBy, quantity, totalPrice) VALUES($1, $2, $3, $4) RETURNING *';
+export const userText = 'INSERT INTO users(firstname, lastname, email, phone, password, address, city, zipcode) VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *';
+export const orderText = 'INSERT INTO orders(menuid, orderedby, quantity, totalprice) VALUES($1, $2, $3, $4) RETURNING *';
 export const find = (selectedColumn, tableName, columnName, value) => (`SELECT ${selectedColumn} FROM ${tableName} WHERE ${columnName} = '${value}'`);
 export const deleteOrder = (id, tableName) => (`DELETE FROM ${tableName} WHERE Id = ${id}`);
 export const menuText = 'INSERT INTO menus(name, description, image, price, categoryId, isAvailable) VALUES($1, $2, $3, $4, $5, $6) RETURNING *';
