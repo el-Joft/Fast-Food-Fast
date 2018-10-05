618f32d45f29b6003757ae9b9b6f1e32cb4f3274
el-joft <ottimothy@gmail.com>
2018-10-05 02:57:05 +0100

    Fixing Bugs during deployment to heroku

--- server/src/migrations/userMigrations.js
+++ server/src/migrations/userMigrations.js
@@ -1,7 +1,6 @@
 import bcrypt from 'bcryptjs';
 import pool from '../config/databaseConfig';
 // import config from '../../../config';
-import {} from 'dotenv/config';
 
 const createUsersTables = () => {
   const queryText = `CREATE TABLE IF NOT EXISTS
@@ -29,7 +28,7 @@ const createUsersTables = () => {
 
   const defaultAdmin = () => {
     const password = '123456';
-    const hashedPassword = bcrypt.hashSync(password, process.env.SALT);
+    const hashedPassword = bcrypt.hashSync(password, 10);
     const adminData = {
       role: 1,
       email: 'ottimothy@gmail.com',
