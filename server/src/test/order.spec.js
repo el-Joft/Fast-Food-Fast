c87f39c60f8b39ef535568c8807895c228b2a1dc
el-joft <ottimothy@gmail.com>
2018-10-05 15:02:26 +0100

    Fixing all bugs relating to the routes and test

--- server/src/test/order.spec.js
+++ server/src/test/order.spec.js
@@ -51,22 +51,24 @@ describe('Test to get all orders', () => {
     });
   });
 
-//   // describe('Test to delete an Order', () => {
-//   //   it('should return 200 if an order was successfully deleted', (done) => {
-//   //     chai.request(app)
-//   //       .delete('/api/v1/orders/32')
-//   //       .end((err, res) => {
-//   //         expect(res).to.have.status(200);
-//   //         done();
-//   //       });
-//   //   });
-//   //   it('should return 404 if parameter is not found', (done) => {
-//   //     chai.request(app)
-//   //       .delete('/api/v1/orders/50000')
-//   //       .end((error, response) => {
-//   //         expect(response).to.have.status(404);
-//   //         done();
-//   //       });
-//   //   });
-// });
+  describe('Test to delete an Order', () => {
+    it('should return 200 if an order was successfully deleted', (done) => {
+      chai.request(app)
+        .delete('/api/v1/orders/4')
+        .set('token', token)
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          done();
+        });
+    });
+    it('should return 404 if parameter is not found', (done) => {
+      chai.request(app)
+        .delete('/api/v1/orders/0')
+        .set('token', token)
+        .end((error, response) => {
+          expect(response).to.have.status(404);
+          done();
+        });
+    });
+  });
 });
